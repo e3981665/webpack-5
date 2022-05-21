98be6e3719b505115e9461d0d793f57e24a8cff5
Adriano Garcia Ramos <adriano.jgrf@gmail.com>
2022-05-21 12:28:46 -0300

    Default

---
+++ dashboard/webpack.dev.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = {
+  entry: {
+    kiwi: "./src/dashboard.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "http://localhost:9000/",
+  },
+  mode: "development",
+  devServer: {
+    port: 9000,
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+    devMiddleware: {
+      index: "dashboard.html",
+      writeToDisk: true,
+    },
+    historyApiFallback: {
+      index: "dashboard.html",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.txt/,
+        type: "asset/source",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+      {
+        test: /\.hbs$/,
+        use: ["handlebars-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ModuleFederationPlugin({
+      name: "App",
+      remotes: {
+        HelloWorldApp: "HelloWorldApp@http://localhost:9001/remoteEntry.js",
+        KiwiApp: "KiwiApp@http://localhost:9002/remoteEntry.js",
+      },
+    }),
+  ],
+};
